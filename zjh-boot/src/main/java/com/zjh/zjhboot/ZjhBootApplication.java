d5acd217b7981b869fb6e1b1bb4c7e564d97c72d
zjh <zhoujunhao@e-future.com.cn>
2018-08-27 16:21:58 +0800

    zjh

---
+++ zjh-boot/src/main/java/com/zjh/zjhboot/ZjhBootApplication.java
@@ -0,0 +1,12 @@
+package com.zjh.zjhboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZjhBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZjhBootApplication.class, args);
+	}
+}
